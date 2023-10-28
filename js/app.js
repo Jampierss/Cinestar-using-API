50e928080ec2fbd8411957b51f4b85b9d20f019e
Jeampierss <jimmyflorian2005@gmail.com>
2023-10-28 00:11:21 -0500

    new version of cinestar, more beauty

--- js/app.js
+++ js/app.js
@@ -1,168 +1,179 @@
-const loadMoviesCartelera = async() => {
+const getMovies = async() => {
     try {
-        const response = await fetch('https://oaemdl.es/cinestar_sweb_php/peliculas/cartelera');
-        console.log(response);
+        
+        const id = new URLSearchParams(window.location.search).get("idx");
+        const response = await fetch(`https://oaemdl.es/cinestar_sweb_php/peliculas/${id}`);
 
         if (response.status === 200) {
-            const data = await response.json();
-            // console.log(data);
-            
-            let movies = '';
-            data.forEach(element => {
-                movies += `
-                    <div class="datos-pelicula">
-                        <h2>${element.Titulo}</h2><br/>
-                        <p>${element.Sinopsis}</p>
-                        <br/>
-                        <div class="boton-pelicula"> 
-                            <a href="pelicula.html?id=${element.id}" >
-                                <img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info"/>
-                            </a>
+
+            const data_json = await response.json();
+            console.log(data_json)
+
+            let html = '<br/><h1>Cartelera</h1><br/>';
+            data_json.forEach(movie => {
+                html += `
+                    <div class="contenido-pelicula">
+                        <div class="datos-pelicula">
+                            <h2>${movie.Titulo}</h2><br/>
+                            <p>${movie.Sinopsis}</p>
+                            <br/>
+                            <div class="boton-pelicula"> 
+                                <a href="pelicula.html?id=${movie.id}" >
+                                    <img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info"/>
+                                </a>
+                            </div>
+                            <div class="boton-pelicula"> 
+                                <a href="https://www.youtube.com/v/${movie.Link}" target=_blank  onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )" >
+                                    <img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer"/>
+                                </a>
+                            </div> 
                         </div>
-                        <div class="boton-pelicula"> 
-                            <a href="https://www.youtube.com/v/${element.Link}" target=_blank  onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )" >
-                                <img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer"/>
-                            </a>
-                        </div> 
+                        <img src="img/pelicula/${movie.id}.jpg" width="160" height="226"/><br/><br/>
                     </div>
-                    <img src="img/pelicula/${element.id}.jpg" width="160" height="226"/><br/><br/>
                 `
             });
+            
+            document.getElementById("contenido-interno").innerHTML = html;
 
-            document.getElementById("contenido-pelicula").innerHTML = movies;
         }
 
-    } catch (error) {
-        console.log(error);
+
+    } catch (err) {
+        console.log(err);
     }
-}
+};
 
-const loadMoviesEstrenos = async() => {
-    try {
-        const response = await fetch('https://oaemdl.es/cinestar_sweb_php/peliculas/estrenos');
-        console.log(response);
+const getMovie = async() => {
 
-        if (response.status === 200) {
-            const data = await response.json();
-            // console.log(data);
-            
-            let movies = '';
-            data.forEach(element => {
-                movies += `
-                    <div class="datos-pelicula">
-                        <h2>${element.Titulo}</h2><br/>
-                        <p>${element.Sinopsis}</p>
-                        <br/>
-                        <div class="boton-pelicula"> 
-                            <a href="pelicula.html?id=${element.id}" >
-                                <img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info"/>
-                            </a>
-                        </div>
-                        <div class="boton-pelicula"> 
-                            <a href="https://www.youtube.com/v/${element.Link}" target=_blank  onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )" >
-                                <img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer"/>
-                            </a>
-                        </div> 
-                    </div>
-                    <img src="img/pelicula/${element.id}.jpg" width="160" height="226"/><br/><br/>
-                `
-            });
+    const id = new URLSearchParams(window.location.search).get('id');
+    const response = await fetch(`https://oaemdl.es/cinestar_sweb_php/peliculas/${id}`);
 
-            document.getElementById("contenido-pelicula").innerHTML = movies;
-        }
+    if (response.status === 200) {
 
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const loadCinemas = async() => {
-    try {
-        const response = await fetch('https://oaemdl.es/cinestar_sweb_php/cines');
-        console.log(response);
-
-        if (response.status === 200) {
-            const data = await response.json();
-            // console.log(data);
-            
-            let cinemas = '';
-            data.forEach(element => {
-                cinemas += `
-                <div class="contenido-cine" id="contenido-cine">
-                    <img src="img/cine/${element.id}.1.jpg" width="227" height="170"/>
-                        <div class="datos-cine">
-                            <h4>${element.RazonSocial}</h4><br/>
-                        <span>${element.Direccion}-${element.Detalle}<br/><br/>Teléfono: ${element.Telefonos} anexo 865</span>
-                    </div>
+        data_json = await response.json();
+        let html = `
+            <br/><h1>Cartelera</h1><br/>
+            <div class="contenido-pelicula">
+                <div class="datos-pelicula">
+                    <h2>${data_json.Titulo}</h2>
+                    <p>${data_json.Sinopsis}</p>
                     <br/>
-                    <a href="cine.html?id=${element.id}">
-                        <img src="img/varios/ico-info2.png" width="150" height="40"/>
-                    </a>
+                    <div class="tabla">
+                        <div class="fila">
+                            <div class="celda-titulo">Título Original :</div>
+                            <div class="celda">${data_json.Titulo}</div>
+                        </div>
+                        <div class="fila">
+                            <div class="celda-titulo">Estreno :</div>
+                            <div class="celda">${data_json.FechaEstrenoss}</div>
+                        </div>
+                        <div class="fila">
+                            <div class="celda-titulo">Género :</div>
+                            <div class="celda">${data_json.Geneross}</div>
+                        </div>
+                        <div class="fila">
+                            <div class="celda-titulo">Director :</div>
+                            <div class="celda">${data_json.Director}</div>
+                        </div>
+                        <div class="fila">
+                            <div class="celda-titulo">Reparto :</div>
+                            <div class="celda">${data_json.Reparto}</div>
+                        </div>
+                    </div>
                 </div>
-                `
-            });
+                <img src="img/pelicula/${data_json.id}.jpg" width="160" height="226"><br/><br/>
+            </div>
+            <div class="pelicula-video">
+                <embed src="https://www.youtube.com/v/${data_json.Link}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
+            </div>
+        `
 
-            document.getElementById("contenido-interno").innerHTML = cinemas;
-        }
-
-    } catch (error) {
-        console.log(error);
+        document.getElementById("contenido-interno").innerHTML = html;
     }
-}
+};
 
-const loadCine = async() => {
-
-    let values = window.location.search;
-    let urlSearchParams = new URLSearchParams(values);
-    let idValue = urlSearchParams.get('id');
-
+const getCinemas = async() => {
     try {
-        const response_general = await fetch('https://oaemdl.es/cinestar_sweb_php/cines/' + idValue);
-        const response_prices = await fetch('https://oaemdl.es/cinestar_sweb_php/cines/' + idValue + '/tarifas');
-        const response_movies = await fetch('https://oaemdl.es/cinestar_sweb_php/cines/' + idValue + '/peliculas');
+    
+        const response = await fetch("https://oaemdl.es/cinestar_sweb_php/cines");
 
-        console.log(response_prices);
-        console.log(response_movies);
-
-        if (response_prices.status === 200 && response_movies.status === 200 && response_general.status === 200) {
-            const data_prices = await response_prices.json();
-            const data_movies = await response_movies.json();
-            const data_general = await response_general.json();
-            // console.log(data);
-
-            let prices = '';
-            data_prices.forEach(element =>{
-                prices += `
-                    <div class="fila">
-                        <div class="celda-titulo">${element.DiasSemana}</div>
-                        <div class="celda">${element.Precio}</div>
+        if (response.status === 200) {
+            
+            const data_json = await response.json();
+            
+            let cinemas = "";
+            data_json.forEach(cinema => {
+                cinemas += `
+                    <div class="contenido-cine" id="contenido-cine">
+                        <img src="img/cine/${cinema.id}.1.jpg" width="227" height="170"/>
+                            <div class="datos-cine">
+                                <h4>${cinema.RazonSocial}</h4><br/>
+                            <span>${cinema.Direccion}-${cinema.Detalle}<br/><br/>Teléfono: ${cinema.Telefonos} anexo 865</span>
+                        </div>
+                        <br/>
+                        <a href="cine.html?id=${cinema.id}">
+                            <img src="img/varios/ico-info2.png" width="150" height="40"/>
+                        </a>
                     </div>
                 `
             });
 
-            let count = 1;
-            let movie = '';
-            data_movies.forEach(element =>{
-                if (count % 2 === 0) {
-                    movie += `
-                        <div class="fila">
-                            <div class="celda-titulo">${element.Titulo}</div>
-                            <div class="celda">${element.Horarios}</div>
-                        </div>
-                    `
-                } else {
-                    movie += `
-                        <div class="fila impar">
-                            <div class="celda-titulo">${element.Titulo}</div>
-                            <div class="celda">${element.Horarios}</div>
-                        </div>
-                    `
-                }
+            document.getElementById('contenido-interno').innerHTML = cinemas;
+        }
 
-                count += 1;
-            });
-            
-            let cine = `
+    } catch (error) { console.log(error) }
+
+};
+
+const getCinema = async() => {
+
+    const id = new URLSearchParams(window.location.search).get('id');
+
+    const response_general = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}`);
+    const response_prices = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/tarifas`);
+    const response_movies = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/peliculas`);
+
+    const responses = [response_general.status, response_movies.status, response_prices.status];
+
+    if (responses.every(status => status === 200)) {
+        
+        const data_general = await response_general.json();
+        const data_movies = await response_movies.json();
+        const data_prices = await response_prices.json();
+
+        let prices = '';
+        data_prices.forEach(price => {
+            prices += `
+                <div class="fila">
+                    <div class="celda-titulo">${price.DiasSemana}</div>
+                    <div class="celda">${price.Precio}</div>
+                </div>
+            `
+        });
+
+        let row_number = 0;
+        let movies = ''; 
+        data_movies.forEach(movie => {
+            if (row_number % 2 === 0) {
+                movies += `
+                    <div class="fila">
+                        <div class="celda-titulo">${movie.Titulo}</div>
+                        <div class="celda">${movie.Horarios}</div>
+                    </div>
+                `
+            } else {
+                movies += `
+                        <div class="fila impar">
+                            <div class="celda-titulo">${movie.Titulo}</div>
+                            <div class="celda">${movie.Horarios}</div>
+                        </div>
+                    `
+            }
+
+            row_number++;
+        });
+
+        let cine = `
                 <h2>${data_general.RazonSocial}</h2>
                 <div class="cine-info">
                     <div class="cine-info datos">
@@ -184,7 +195,7 @@ const loadCine = async() => {
                                 <div class="celda-cabecera">Películas</div>
                                 <div class="celda-cabecera">Horarios</div>
                             </div>
-                            ${movie}
+                            ${movies}
                         </div>
                     </div>
                 </div>
@@ -198,98 +209,22 @@ const loadCine = async() => {
                         <b>CINESTAR</b>, siempre pensando en tí. 
                     </span>		
                 </div>
-
             `
-                
-            
 
-            document.getElementById("contenido-interno").innerHTML = cine;
-        }
+            document.getElementById('contenido-interno').innerHTML = cine;
 
-    } catch (error) {
-        console.log(error);
+
+
     }
-}
+};
 
-const loadMovie = async() => {
-
-    let values = window.location.search;
-    let urlSearchParams = new URLSearchParams(values);
-    let idValue = urlSearchParams.get('id');
-
-    try {
-        const response = await fetch('https://oaemdl.es/cinestar_sweb_php/peliculas/' + idValue);
-
-        if (response.status === 200) {
-            const data = await response.json();
-            // console.log(data);
-
-            let movie = `
-                <br/><h1>Cartelera</h1><br/>
-                <div class="contenido-pelicula">
-                    <div class="datos-pelicula">
-                        <h2>${data.Titulo}</h2>
-                        <p>${data.Sinopsis}</p>
-                        <br/>
-                        <div class="tabla">
-                            <div class="fila">
-                                <div class="celda-titulo">Título Original :</div>
-                                <div class="celda">${data.Titulo}</div>
-                            </div>
-                            <div class="fila">
-                                <div class="celda-titulo">Estreno :</div>
-                                <div class="celda">${data.FechaEstrenoss}</div>
-                            </div>
-                            <div class="fila">
-                                <div class="celda-titulo">Género :</div>
-                                <div class="celda">${data.Geneross}</div>
-                            </div>
-                            <div class="fila">
-                                <div class="celda-titulo">Director :</div>
-                                <div class="celda">${data.Director}</div>
-                            </div>
-                            <div class="fila">
-                                <div class="celda-titulo">Reparto :</div>
-                                <div class="celda">${data.Reparto}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <img src="img/pelicula/${data.id}.jpg" width="160" height="226"><br/><br/>
-                </div>
-                <div class="pelicula-video">
-                    <embed src="https://www.youtube.com/v/${data.Link}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
-                </div>
-            `;
-
-            document.getElementById("contenido-interno").innerHTML = movie;
-        }
-
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 const currentURL = window.location.href;
-const parts = currentURL.split('/');
-const lastPart = parts[parts.length - 1];
-console.log(lastPart);
+const partsURL = currentURL.split('/');
+const lastPartURL = partsURL[partsURL.length - 1]; 
+// console.log(lastPart); // peliculas.html?idx=cartelera
 
-const values = window.location.search;
-const urlSearchParams = new URLSearchParams(values);
-const idxValue = urlSearchParams.get('idx');
-
-console.log(idxValue);
-
-if (idxValue === "cartelera") {
-    loadMoviesCartelera();
-} else if (idxValue === "estrenos") {
-    loadMoviesEstrenos();
-}
-
-if (lastPart === "cines.html") {
-    loadCinemas();
-} else if (lastPart.includes("cine.html")) {
-    loadCine();
-} else if (lastPart.includes("pelicula.html")) {
-    loadMovie();
-}
+if (lastPartURL.includes('cines.html')) { getCinemas(); }
+else if (lastPartURL.includes('cine.html')) { getCinema(); }
+else if (lastPartURL.includes('peliculas.html')) { getMovies(); }
+else if (lastPartURL.includes('pelicula.html')) { getMovie(); }
